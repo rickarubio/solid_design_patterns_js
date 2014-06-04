b11a6ed4c119a1da4eac4dd939c43f4f21170e84
Ricardo Rubio <rickarubio@gmail.com>
2014-06-03 18:23:03 -0700

    add code for dynamic manipulation of objects section

---
+++ js/lib/timed_task.js
@@ -0,0 +1,6 @@
+var TimedTask = function(options) {
+  options = options || {};
+  var that = new Task(options);
+  that.startingDate = options.startingDate || new Date().getTime();
+  return that;
+}
